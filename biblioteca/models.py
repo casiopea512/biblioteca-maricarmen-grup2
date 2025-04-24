6465ca1fb78bb3827177d321c7d344f52011823d
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-04-24 12:19:53 +0200

    [[6 busqueda - IN PROGRESS] - añadido el centro en el ejemplar (modelo) - cambios en los endpoints y limpieza de la api

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
 from django.contrib.auth.hashers import make_password
 
+class Centre(models.Model):
+    nom = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nom
 
 class Categoria(models.Model):
     class Meta:
@@ -90,6 +94,7 @@ class Exemplar(models.Model):
     registre = models.CharField(max_length=100,null=True,blank=True)
     exclos_prestec = models.BooleanField(default=True)
     baixa = models.BooleanField(default=False)
+    centre = models.ForeignKey(Centre, on_delete=models.SET_NULL, null=True, blank=True)
     def __str__(self):
         return "REG:{} - {}".format(self.registre,self.cataleg.titol)
 
@@ -99,11 +104,6 @@ class Imatge(models.Model):
 
 
 # Usuaris
-
-class Centre(models.Model):
-    nom = models.CharField(max_length=200)
-    def __str__(self):
-        return self.nom
 
 class Cicle(models.Model):
     nom = models.CharField(max_length=200)
